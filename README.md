68505d549a06c3f6e2a833c9a11565f3ceefbaf6
Philip Axer <axer@ida.ing.tu-bs.de>
2013-02-24 01:01:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+pygraphviz
+==========
+
+Python3 compatible (and only python3 compatible) pygraphviz port.
---
+++ pygraphviz/version.py
@@ -0,0 +1,11 @@
+"""
+Version information for PyGraphviz, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+__version__ = '1.2.dev'
+__revision__ = None
+__date__ = 'Sat Feb 23 21:41:32 2013'
+
